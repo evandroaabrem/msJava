5891c04bbbcd736242fd739f5411cbbe745dcc95
eaabreu@fitec.org.br <eaabreu@fitec.org.br>
2022-07-04 12:02:39 -0300

    refactor

--- backEnd/integracao/src/main/java/br/com/otima/IntegracaoApplication.java
+++ backEnd/integracao/src/main/java/br/com/otima/IntegracaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IotOtimaWebApplication {
+public class IntegracaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IotOtimaWebApplication.class, args);
+		SpringApplication.run(IntegracaoApplication.class, args);
 	}
 
 }
